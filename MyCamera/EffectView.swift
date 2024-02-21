c9e102713472a27895e8a767c308876c1fce3211
medyr <haru68346@gmail.com>
2024-02-21 22:19:20 +0900

    2-3finish

---
+++ MyCamera/EffectView.swift
@@ -0,0 +1,18 @@
+//
+//  EffectView.swift
+//  MyCamera
+//
+//  Created by ゆり on 2024/02/19.
+//
+
+import SwiftUI
+
+struct EffectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EffectView()
+}
